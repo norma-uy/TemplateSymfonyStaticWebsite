9d1442a1d5dabca342f8e8c9711e729fdce4730c
samuel <42385434+samuelV102@users.noreply.github.com>
2022-12-13 14:05:03 -0300

    - Instalación de sass, postcss y autoprefixer. - Instalación de eslint y stylelint. - Instalación de prettier. - Instalación de jquery y bootstrap. - Scripts comandos para formatear, arreglar y auditar el código fuente con prettier, eslint y stylelint. - Desinstalación de stimulus. - Plantilla básica.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require( "@symfony/webpack-encore" );
+
+/// ///////////// WEBSITE CONFIG //////////////////
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+if ( !Encore.isRuntimeEnvironmentConfigured() ) {
+    Encore.configureRuntimeEnvironment( process.env.NODE_ENV || "dev" );
 }
 
 Encore
+
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath( "public/build/website/" )
+
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath( "/build/website/" )
+
     // only needed for CDN's or subdirectory deploy
-    //.setManifestKeyPrefix('build/')
+    // .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
@@ -20,10 +25,30 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry( "app", "./assets/website/index.js" )
+    .addStyleEntry( "global", "./assets/website/styles/index.scss" )
+
+    .copyFiles( {
+        from: "./assets/website/images",
+
+        // optional target path, relative to the output dir
+        to: "images/[path][name].[ext]"
+
+        // if versioning is enabled, add the file hash too
+        // to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        // pattern: /\.(png|jpg|jpeg)$/
+    } )
+
+    .configureFontRule( {
+        type: "asset"
+
+        // maxSize: 4 * 1024
+    } )
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    // .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -41,9 +66,10 @@ Encore
      */
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
+    .enableSourceMaps( !Encore.isProduction() )
+
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning( Encore.isProduction() )
 
     // configure Babel
     // .configureBabel((config) => {
@@ -51,26 +77,36 @@ Encore
     // })
 
     // enables and configure @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
-    })
+    .configureBabelPresetEnv( ( config ) => {
+        config.useBuiltIns = "usage";
+        config.corejs = "3.23";
+    } )
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    // .enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    // .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    // .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
+    .autoProvidejQuery();
 
-module.exports = Encore.getWebpackConfig();
+// build the first configuration
+const websiteConfig = Encore.getWebpackConfig();
+
+// Set a unique name for the config (needed later!)
+websiteConfig.name = "website-config";
+
+// reset Encore to build the second config
+Encore.reset();
+
+/// ////////////////////////////////////////////////////
+module.exports = [ websiteConfig ];
